a4a176aeebd2ae7b50d8872321ef6917bf14ed33
tu10ng <2059734099@qq.com>
2022-11-23 15:53:28 +0800

    basic editing environment done
    lsp and corfu and many others.

--- .custom.el
+++ .custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit-delta magit consult corfu-doc cape kind-icon orderless corfu meow eros ace-window which-key avy systemd hl-todo vertico module use-package)))
+   '(quickrun consult-eglot ox-reveal all-the-icons doct org-super-agenda valign org-appear org-modern shackle wakatime-mode restart-emacs pyim-basedict pyim prettify-math prettify-greek rainbow-delimiters helpful gcmh flycheck selectric-mode selectic-mode info-colors magit-delta magit consult corfu-doc cape kind-icon orderless corfu meow eros ace-window which-key avy systemd hl-todo module))
+ '(wakatime-api-key "c2a0b2a6-ab7d-4af3-bfbc-0924271ecbb6")
+ '(wakatime-cli-path "/home/qb/.wakatime/wakatime-cli"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
