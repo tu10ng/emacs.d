0c5caffb69289fabfb6db3894b78e3021fd53add
tu10ng <2059734099@qq.com>
2022-12-04 03:09:49 +0800

    fine(orderless, paren, magit)

--- .custom.el
+++ .custom.el
@@ -13,4 +13,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(show-paren-match ((t (:background "#1B2229" :foreground "#ffffff" :underline t :weight ultra-bold)))))
