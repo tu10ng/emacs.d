4a0341c5c77abb52b426d875202c17a0ec627f0f
tu10ng <2059734099@qq.com>
2022-12-04 15:42:40 +0800

    fine(corfu doc): corfu-popupinfo-mode

--- .custom.el
+++ .custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/org/gtd.org"))
  '(package-selected-packages
-   '(speed-type marginalia vertico-directory fish-mode fish python-black py-yapf yapfify tldr quickrun consult-eglot ox-reveal all-the-icons doct org-super-agenda valign org-appear org-modern shackle wakatime-mode restart-emacs pyim-basedict pyim prettify-math prettify-greek rainbow-delimiters helpful gcmh flycheck selectric-mode selectic-mode info-colors magit-delta magit consult corfu-doc cape kind-icon orderless corfu meow eros ace-window which-key avy systemd hl-todo module))
+   '(speed-type marginalia vertico-directory fish-mode fish python-black py-yapf yapfify tldr quickrun consult-eglot all-the-icons doct org-super-agenda valign org-appear shackle wakatime-mode restart-emacs pyim-basedict pyim prettify-math prettify-greek rainbow-delimiters gcmh selectric-mode selectic-mode info-colors magit-delta meow eros which-key avy systemd hl-todo module))
  '(wakatime-api-key "c2a0b2a6-ab7d-4af3-bfbc-0924271ecbb6")
  '(wakatime-cli-path "/home/qb/.wakatime/wakatime-cli"))
 (custom-set-faces
