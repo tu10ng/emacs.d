9cd6c809ce5fb164589b12381e41d4d61fcef1f9
tu10ng <2059734099@qq.com>
2023-01-06 18:48:38 +0800

    commit by magit in emacs@2023-01-06 18:48:38

--- .custom.el
+++ .custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(bison-mode exec-path-from-shell haskell-mode markdown-mode smart-hungry-delete rust-mode benchmark-init ox-reveal speed-type helpful magit meow move-text consult-dir magit-todos mood-line youdao-dictionary doom-modeline ahk-mode vertico-directory fish-mode fish python-black py-yapf yapfify tldr quickrun consult-eglot all-the-icons doct org-super-agenda valign org-appear shackle wakatime-mode restart-emacs pyim-basedict pyim prettify-math prettify-greek rainbow-delimiters gcmh selectric-mode selectic-mode info-colors magit-delta eros which-key avy systemd hl-todo module))
+   '(clojure-mode-extra-font-locking cider clojure-mode bison-mode exec-path-from-shell haskell-mode markdown-mode smart-hungry-delete rust-mode benchmark-init ox-reveal speed-type helpful magit meow move-text consult-dir magit-todos mood-line youdao-dictionary doom-modeline ahk-mode vertico-directory fish-mode fish python-black py-yapf yapfify tldr quickrun consult-eglot all-the-icons doct org-super-agenda valign org-appear shackle wakatime-mode restart-emacs pyim-basedict pyim prettify-math prettify-greek rainbow-delimiters gcmh selectric-mode selectic-mode info-colors magit-delta eros which-key avy systemd hl-todo module))
+ '(safe-local-variable-values '((checkdoc-minor-mode . t)))
  '(wakatime-api-key "c2a0b2a6-ab7d-4af3-bfbc-0924271ecbb6")
  '(wakatime-cli-path "/usr/bin/wakatime-cli"))
 (custom-set-faces
