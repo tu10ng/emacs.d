5e7a02c9d8b2e93e3215b49dae26f899bb0c964d
tu10ng <2059734099@qq.com>
2023-02-10 04:49:22 +0800

    commit by tu10ng in emacs@2023-02-10 04:49:22

--- .custom.el
+++ .custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(tuareg ebnf-mode ebnf keycast clojure-mode-extra-font-locking cider clojure-mode bison-mode exec-path-from-shell haskell-mode markdown-mode smart-hungry-delete rust-mode benchmark-init ox-reveal speed-type helpful magit meow move-text consult-dir magit-todos mood-line youdao-dictionary doom-modeline ahk-mode vertico-directory fish-mode fish python-black py-yapf yapfify tldr quickrun consult-eglot all-the-icons doct org-super-agenda valign org-appear shackle wakatime-mode restart-emacs pyim-basedict pyim prettify-math prettify-greek rainbow-delimiters gcmh selectric-mode selectic-mode info-colors magit-delta eros which-key avy systemd hl-todo module))
+   '(sly slime try tuareg ebnf-mode ebnf keycast clojure-mode-extra-font-locking cider clojure-mode bison-mode exec-path-from-shell haskell-mode markdown-mode smart-hungry-delete rust-mode benchmark-init ox-reveal speed-type helpful magit meow move-text consult-dir magit-todos mood-line youdao-dictionary doom-modeline ahk-mode vertico-directory fish-mode fish python-black py-yapf yapfify tldr quickrun consult-eglot all-the-icons doct org-super-agenda valign org-appear shackle wakatime-mode restart-emacs pyim-basedict pyim prettify-math prettify-greek rainbow-delimiters gcmh selectric-mode selectic-mode info-colors magit-delta eros which-key avy systemd hl-todo module))
  '(safe-local-variable-values '((checkdoc-minor-mode . t)))
  '(wakatime-api-key "c2a0b2a6-ab7d-4af3-bfbc-0924271ecbb6")
  '(wakatime-cli-path "/usr/bin/wakatime-cli"))
@@ -13,4 +13,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(show-paren-match ((t (:background "#1B2229" :foreground "#ffffff" :underline t :weight ultra-bold)))))
+ '(show-paren-match ((t (:background "#1B2229" :foreground "#ffffff" :underline t :weight ultra-bold))))
+ '(show-paren-mismatch ((t (:background "purple")))))
