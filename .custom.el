1a98363c9ba93f84bf950b89b1d0ed8793257b25
tu10ng <2059734099@qq.com>
2023-03-29 11:03:24 +0800

    commit by tu10ng in emacs@2023-03-29 11:03:24

--- .custom.el
+++ .custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(pdf-tools iscroll graphviz-dot-mode cdlatex auctex lox-mode posframe electric-operator try tuareg ebnf-mode ebnf clojure-mode-extra-font-locking bison-mode exec-path-from-shell smart-hungry-delete benchmark-init ox-reveal helpful move-text consult-dir youdao-dictionary doom-modeline ahk-mode vertico-directory fish-mode fish python-black py-yapf yapfify all-the-icons org-super-agenda valign org-appear shackle wakatime-mode restart-emacs pyim-basedict prettify-math prettify-greek rainbow-delimiters gcmh selectric-mode selectic-mode info-colors magit-delta which-key avy module))
+   '(nasm-mode racket-mode pdf-tools iscroll graphviz-dot-mode cdlatex auctex lox-mode posframe electric-operator try tuareg ebnf-mode ebnf clojure-mode-extra-font-locking bison-mode exec-path-from-shell smart-hungry-delete benchmark-init ox-reveal helpful move-text consult-dir youdao-dictionary doom-modeline ahk-mode vertico-directory fish-mode fish python-black py-yapf yapfify all-the-icons org-super-agenda valign org-appear shackle wakatime-mode restart-emacs pyim-basedict prettify-math prettify-greek rainbow-delimiters gcmh selectric-mode selectic-mode info-colors magit-delta which-key avy module))
  '(safe-local-variable-values '((checkdoc-minor-mode . t)))
  '(wakatime-api-key "c2a0b2a6-ab7d-4af3-bfbc-0924271ecbb6")
  '(wakatime-cli-path "/usr/bin/wakatime-cli"))
